c8b96d65c379d56f011b61fefcaf8e7b1814c6e9
MohamedGabal <gabalmohamed33@gmail.com>
2023-05-21 00:43:05 +0300

    thirdCommit

--- middleware/losted.validations.js
+++ middleware/losted.validations.js
@@ -2,10 +2,13 @@ const Joi = require("joi");
 
 const schema=Joi.object({
     email:Joi.string().required().email(),
-    fullName:Joi.string().required(),
-    phone_number:Joi.string().required(),
-    overviwe:Joi.string().required().min(50).max(500),
-    chieldAge:Joi.string().required(),
+    full_name:Joi.string().required(),
+    phone:Joi.string().required(),
+    dwescribtio:Joi.string().required().min(50).max(500),
+    type:Joi.string().required(),
+     state:Joi.string().required(),
+     userId:Joi.string().required(),
+     idNumber:Joi.string().required(),
     
 })
 
