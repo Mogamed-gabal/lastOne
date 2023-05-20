00da01d67787d707ee936a76c508d35cc2912c6e
MohamedGabal <gabalmohamed33@gmail.com>
2023-05-21 02:52:00 +0300

    sevinthCommit

--- middleware/things.validation.js
+++ middleware/things.validation.js
@@ -4,6 +4,9 @@ const schema=Joi.object({
     full_name:Joi.string().required().min(10).max(20),
     email:Joi.string().email().required(),
     userId:Joi.string().required(),
+    idNumber:Joi.string().required(),
+    phone:Joi.string().required(),
+    state:Joi.string(),
     dwescribtio:Joi.string().required().min(50).max(500),
 })
 module.exports.thingsRebortValidations=async (req,res,next)=>{
